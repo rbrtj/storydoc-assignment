04ffe2afd678a273c1bbd07c04533a03e669aaf6
rbrtj <robertjaszczurek@icloud.com>
2023-10-05 16:23:05 +0200

    WorkspacesHeader functionalities

--- src/components/workspacesHeader/WorkspacesHeader.tsx
+++ src/components/workspacesHeader/WorkspacesHeader.tsx
@@ -1,38 +1,52 @@
 import "./WorkspacesHeader.scss";
 import { Button } from "../ui/button";
-import { Logo, LogoEmpty } from "../../assets/icons";
-import { useDispatch, useSelector } from "react-redux";
-import { boardSlice } from "../../store/slices";
+import { Check, Logo, LogoEmpty, Plus } from "../../assets/icons";
+import { useSelector, useDispatch } from "react-redux";
+import { addBoard, setBoardActive } from "../../store/slices";
 import { useState } from "react";
+import { RootState } from "../../store/store.ts";
 
 export const WorkspacesHeader = () => {
-  const [newBoardName, setNewBoardName] = useState("Workspace name");
+  const [newBoardName, setNewBoardName] = useState("");
   const [showNewBoardItem, setShowNewBoardItem] = useState(false);
-  const dispatch = useDispatch();
-  const boards = useSelector((state) => state.board);
 
-  const handleButtonClick = () => {
+  const dispatch = useDispatch();
+  const boards = useSelector((state: RootState) => state.board);
+  const handleCreateNewBoard = () => {
     setShowNewBoardItem(true);
-    dispatch(boardSlice.actions.setBoardActive({ index: null }));
+    dispatch(setBoardActive({ index: null }));
   };
 
   const handleCancelNewBoard = () => {
     setShowNewBoardItem(false);
   };
 
+  const handleAddNewBoard = () => {
+    dispatch(
+      addBoard({
+        name: newBoardName,
+      }),
+    );
+    setShowNewBoardItem(false);
+  };
+
+  const handleSetActiveBoard = (index: number) => {
+    if (!showNewBoardItem) {
+      dispatch(setBoardActive({ index }));
+    }
+  };
+
   return (
     <div className="workspaces-header">
-      {boards.map((board, index) => (
+      {boards.map((board, index: number) => (
         <div
           key={index}
           className={`workspaces-header__item ${
             board.isActive ? "workspaces-header__item--active" : ""
-          }`}
-          onClick={() => {
-            if (!showNewBoardItem) {
-              dispatch(boardSlice.actions.setBoardActive({ index }));
-            }
-          }}
+          }
+          ${!showNewBoardItem ? "cursor-pointer" : ""}
+          `}
+          onClick={() => handleSetActiveBoard(index)}
         >
           <Logo />
           {board.name}
@@ -40,16 +54,30 @@ export const WorkspacesHeader = () => {
       ))}
       {showNewBoardItem && (
         <div className="workspaces-header__item workspaces-header__item--new">
-          <LogoEmpty onClick={handleCancelNewBoard} />
+          <LogoEmpty onClick={() => handleCancelNewBoard()} />
           <input
             value={newBoardName}
             onChange={(e) => setNewBoardName(e.target.value)}
             className="workspaces-header__item__input"
+            placeholder="Workspace name"
           />
         </div>
       )}
-
-      <Button onClick={handleButtonClick} />
+      {showNewBoardItem ? (
+        <Button
+          variant="primary"
+          onClick={() => handleAddNewBoard()}
+          disabled={newBoardName.length === 0}
+        >
+          <Check />
+          Save new workspace
+        </Button>
+      ) : (
+        <Button onClick={() => handleCreateNewBoard()} variant="default">
+          <Plus />
+          Create Workspace
+        </Button>
+      )}
     </div>
   );
 };
