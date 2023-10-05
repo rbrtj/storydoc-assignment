632d19975e11f36df9350f1d5e765ef4028d2c68
rbrtj <robertjaszczurek@icloud.com>
2023-10-05 16:22:20 +0200

    Add button variants

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,10 +1,32 @@
 import "./Button.scss";
 import { Plus } from "../../../assets/icons";
-export const Button = ({ onClick }) => {
+
+const ButtonVariants = {
+  default: "button-default",
+  primary: "button-primary",
+};
+
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+  variant: keyof typeof ButtonVariants;
+  disabled?: boolean;
+}
+export const Button = ({
+  onClick,
+  children,
+  variant,
+  disabled,
+}: ButtonProps) => {
   return (
-    <button onClick={onClick} className="button">
-      <Plus />
-      Create workspace
+    <button
+      onClick={onClick}
+      className={`button ${ButtonVariants[variant]} ${
+        disabled ? "disabled" : ""
+      }`}
+      disabled={disabled}
+    >
+      {children}
     </button>
   );
 };
