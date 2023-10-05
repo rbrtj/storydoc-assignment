a67c1d1a8ddba0137ebbdc3aac844f8376753448
rbrtj <robertjaszczurek@icloud.com>
2023-10-05 22:24:44 +0200

    MainView update to render lists for active workspace

--- src/components/mainView/MainView.tsx
+++ src/components/mainView/MainView.tsx
@@ -1,10 +1,14 @@
 import "./MainView.scss";
 import { TasksList } from "../tasksList";
+import { useListsForActiveWorkspace } from "../../hooks/useGetListsForActiveWorkspace";
 
 export const MainView = () => {
+  const tasksLists = useListsForActiveWorkspace();
   return (
     <div className="main-view">
-      <TasksList />
+      {tasksLists.map((list, index) => (
+        <TasksList list={list} listIndex={index} key={index} />
+      ))}
     </div>
   );
 };
